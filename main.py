7af9c9f6f141b381d5594371e34494ef988603eb
toba <toba1610@googlemail.com>
2025-08-11 15:22:26 +0200

    implemented multiple broker support

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ import re
 
 IPV4_PATTERN = "^(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 
-def send_payload(client: mqtt.Client, dummys:int, delay: float):
+number_of_repetations = 1
+payload: dict
+config: dict
+client_list: list = []
+
+def send_payload(client_list: list[mqtt.Client], dummys:int, delay: float, nore: int, payload):
 
     '''
         Handels the modification of the payload and starts the next timer for the next message
@@ -21,27 +26,23 @@ def send_payload(client: mqtt.Client, dummys:int, delay: float):
             None
     '''
 
-    with open('topics.json', 'r') as payload_file:
-        payload = json.load(payload_file)
-    
-    with open('config.json', 'r') as config_file:
-        config = json.load(config_file)
+    payload = modify_timestamp(payload=payload, offset=config['offset'], nore=nore)
 
-    payload = modify_timestamp(payload=payload, offset=config['offset'])
+    # with open('topics.json', 'w', encoding='utf-8') as payload_file:
+    #     json.dump(payload, payload_file, indent=4)
 
-    with open('topics.json', 'w', encoding='utf-8') as payload_file:
-        json.dump(payload, payload_file, indent=4)
+    for client in client_list:
 
-    for device in range(1,dummys+1):
+        for device in range(1,dummys+1):
 
-        payload = modify_serial(payload=payload)
+            # payload = modify_serial(payload=payload)
 
-        for key, value in payload["Topics"].items():
+            for key, value in payload["Topics"].items():
 
-            client.publish(key, json.dumps(value), qos=1)
-            print(f'Message send')
+                client.publish(key, json.dumps(value), qos=1)
+                print(f'Message send to {client}')
 
-    threading.Timer(delay, send_payload, args=(client, dummys, delay)).start()
+    threading.Timer(delay, send_payload, args=(client_list, dummys, delay, number_of_repetations, payload)).start()
 
 def modify_serial(payload: dict)->dict:
 
@@ -66,7 +67,7 @@ def modify_serial(payload: dict)->dict:
     payload["Topics"] = new_topics
     return payload
 
-def modify_timestamp(payload:dict, offset:int)->dict:
+def modify_timestamp(payload:dict, offset:int, nore:int)->dict:
 
     '''
         Increments the timestamp (`ts`) by 1 for each topic in the payload.
@@ -81,7 +82,7 @@ def modify_timestamp(payload:dict, offset:int)->dict:
 
     for key, value in payload["Topics"].items():
         
-        value['ts'] = value['ts'] + offset
+        value['ts'] = value['ts'] + (offset * nore)
 
     return payload
 
@@ -89,25 +90,48 @@ def modify_timestamp(payload:dict, offset:int)->dict:
 
 if __name__ == "__main__":
     
-    with open('config.json', 'r') as config_file:
-        config = json.load(config_file)
-
     client = mqtt.Client(CallbackAPIVersion.VERSION2)
 
-    if config.get("username") and config.get("password"):
-        client.username_pw_set(config["username"], config["password"])
+    with open('topics.json', encoding='utf-8') as payload_file:
+        payload = json.load(payload_file)
 
-    if re.match(IPV4_PATTERN,config["IP"]):
-        if config['Port'] in range(1, 65535):
-            client.connect(config["IP"], config["Port"])
-            print(f'Connected to Broker')
-        else:
-            print(f'Port in config.json not valid: {config["Port"]}')
-    else:
-        print(f'IP in config.json not valid: {config["IP"]}')
+    with open('config.json', encoding='utf-8') as config_file:
+        config = json.load(config_file)
+
+    # if config.get("username") and config.get("password"):
+    #     client.username_pw_set(config["username"], config["password"])
+
+    # if re.match(IPV4_PATTERN,config["IP"]):
+    #     if config['Port'] in range(1, 65535):
+    #         client.connect(config["IP"], config["Port"])
+    #         print(f'Connected to Broker')
+    #     else:
+    #         print(f'Port in config.json not valid: {config["Port"]}')
+    # else:
+    #     print(f'IP in config.json not valid: {config["IP"]}')
 
     interval = config['interval']
 
-    threading.Timer(interval, send_payload, args=(client,config['modules'], interval)).start()
+    for ip in config["IP"]:
+        if re.match(IPV4_PATTERN,ip):
+            if config['Port'] in range(1, 65535):
+                
+                client = mqtt.Client(client_id=f"client_{ip}", callback_api_version=CallbackAPIVersion.VERSION2)
+                if config.get("username") and config.get("password"):
+                    client.username_pw_set(config["username"], config["password"])
+                client.connect(ip, config["Port"])
+                print(ip)
+                client.loop_start()
+                client_list.append(client)
+
+                print(f'Connected to Broker')
+            else:
+                print(f'Port in config.json not valid: {config["Port"]}')
+        else:
+            print(f'IP in config.json not valid: {ip}')        
+
+    
+
+    threading.Timer(interval, send_payload, args=(client_list,config['modules'], interval, number_of_repetations, payload)).start()
 
     client.loop_start()
